00740843c3e45ec8ac4761adc00a4d94af53bed8
Barbara Peric <b.peric10@gmail.com>
2024-03-10 12:01:46 +0000

    make responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,21 @@ export default function Home() {
         <div className={styles.wrapper}>
             <div className={`${styles.box} ${styles['bar-top']}`}></div>
             <div className={`${styles.box} ${styles['bar-left']}`}></div>
+            <div className={`${styles.box} ${styles['title']}`}>
+                <h1>Experiments with desing layouts</h1>
+            </div>
+            <div className={`${styles.box} ${styles['subtitle']}`}>
+                <p>I am a subtitle</p>
+            </div>
             <div className={`${styles.box} ${styles['content-a']}`}>
-                <p>Experiments with desing layouts</p>
+                <h1>I am a side text</h1>
             </div>
             <div className={`${styles.box} ${styles['content-b']}`}>
-                <p>I am a side text</p>
+                <p>
+                    I am a longer text Lorem ipsum dolor sit amet consectetur,
+                    adipisicing elit.
+                </p>
             </div>
-            <div className={`${styles.box} ${styles['content-c']}`}></div>
-            <div className={`${styles.box} ${styles['content-d']}`}></div>
             <div className={`${styles.box} ${styles['bar-right-a']}`}></div>
             <div className={`${styles.box} ${styles['bar-right-b']}`}></div>
         </div>
