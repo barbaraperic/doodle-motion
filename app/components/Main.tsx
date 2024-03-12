59fa01b5c6dc6c7302bbe42c4172de26f4b83e7a
Barbara Peric <b.peric10@gmail.com>
2024-03-12 10:10:41 +0000

    header grid with dots and rotated bar

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -5,9 +5,11 @@ export default function Main() {
         <div className={`${styles['main-wrapper']} grid-wrapper`}>
             <div className={`${styles['bar-a']}`}></div>
             <div className={`${styles['bar-b']}`}></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={`${styles['rotated-text']}`}>
+                <p>Some text</p>
+            </div>
+            <div className={`${styles['dot-a']}`}></div>
+            <div className={`${styles['dot-b']}`}></div>
             <div></div>
             <div></div>
         </div>
